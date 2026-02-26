ec3f15f8829fb92cc367a1dafbc829570b345154
Robin Strand Prestmo <robinsp@stud.ntnu.no>
2026-02-26 21:53:32 +0100

    Added Exchange class and constructor

--- src/main/java/edu/ntnu/idatt2003/group16/exchange/Exchange.java
+++ src/main/java/edu/ntnu/idatt2003/group16/exchange/Exchange.java
@@ -1,4 +1,51 @@
 package edu.ntnu.idatt2003.group16.exchange;
 
+import edu.ntnu.idatt2003.group16.investment.Stock;
+
+import java.util.*;
+
 public class Exchange {
+  private String name;
+  private int week;
+  private Map<String, Stock> stockMap;
+  private Random random;
+
+  public Exchange(String name, List<Stock> stocks) {
+    if (name == null || name.isBlank()) {
+      throw new IllegalArgumentException("Name cannot be null or blank.");
+    }
+
+    if (stocks == null) {
+      throw new IllegalArgumentException("Stocks cannot be null.");
+    }
+
+    this.name = name;
+    this.week = 0;
+    this.stockMap = new HashMap<>();
+    this.random = new Random();
+
+    for (Stock stock : stocks) {
+      if (stock == null) {
+        throw new IllegalArgumentException("Stock cannot be null.");
+      }
+      stockMap.put(stock.getSymbol(), stock);
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
