373edb6fe52cfd0bf5e3a04a0b365e72af003d1b
Odin Grav <odin@grav.name>
2026-02-28 01:21:53 +0100

    Wrote unit tests for claculate methods in PurchaseCalculator class

--- src/test/java/edu/ntnu/idatt2003/group16/transaction/calculator/PurchaseCalculatorTest.java
+++ src/test/java/edu/ntnu/idatt2003/group16/transaction/calculator/PurchaseCalculatorTest.java
@@ -1,44 +1,63 @@
 package edu.ntnu.idatt2003.group16.transaction.calculator;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import edu.ntnu.idatt2003.group16.investment.Share;
 import edu.ntnu.idatt2003.group16.investment.Stock;
-import org.junit.jupiter.api.BeforeEach;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-
-import static org.junit.jupiter.api.Assertions.*;
 class PurchaseCalculatorTest {
+  private static Share share;
+  private static BigDecimal expectedGrossValue;
+  private static BigDecimal expectedCommission;
+  private static BigDecimal expectedTax;
+  private static BigDecimal expectedTotal;
+  private static PurchaseCalculator purchaseCalculator;
 
-  @BeforeEach
-  void setUp() {
-    }
+  @BeforeAll
+  static void setUp() {
+    BigDecimal purchasePrice = new BigDecimal("3.15");
+    Stock stock = new Stock(
+        "GOOG", "Alphabet Inc Class C", purchasePrice);
+
+    BigDecimal quantity = new BigDecimal("3");
+    share = new Share(stock, quantity, purchasePrice);
+
+    purchaseCalculator = new PurchaseCalculator(share);
+
+    expectedGrossValue = purchasePrice.multiply(quantity);
+    expectedCommission = expectedGrossValue.multiply(new BigDecimal("0.005"));
+    expectedTax = new BigDecimal("0");
+    expectedTotal = expectedGrossValue.add(expectedCommission).add(expectedTax);
+  }
 
   @Test
   void constructorTest() {
-    Stock testStock = new Stock(
-        "GOOG", "Alphabet Inc Class C", new BigDecimal("3.15"));
-    BigDecimal quantity = new BigDecimal("1");
-
-    Share testShare = new Share(testStock, quantity, testStock.getSalesPrice().multiply(quantity));
-
-    assertDoesNotThrow(() -> new PurchaseCalculator(testShare));
+    assertDoesNotThrow(() -> new PurchaseCalculator(share));
     assertThrows(IllegalArgumentException.class, () -> new PurchaseCalculator(null));
   }
 
   @Test
   void calculateGross() {
-    }
+    assertEquals(expectedGrossValue, purchaseCalculator.calculateGross());
+  }
 
   @Test
   void calculateCommission() {
-    }
+    assertEquals(expectedCommission, purchaseCalculator.calculateCommission());
+  }
 
   @Test
   void calculateTax() {
-    }
+    assertEquals(expectedTax, purchaseCalculator.calculateTax());
+  }
 
   @Test
   void calculateTotal() {
-    }
+    assertEquals(expectedTotal, purchaseCalculator.calculateTotal());
+  }
 }
