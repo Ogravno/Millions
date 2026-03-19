4586ab0e9a0c793910b75279c7b844691eee3c6c
Odin Grav <odin@grav.name>
2026-03-19 13:27:28 +0100

    test fails

---
+++ src/test/java/edu/ntnu/idatt2003/group16/player/PlayerTest.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idatt2003.group16.player;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class PlayerTest {
+
+  @Test
+  void addMoney() {
+    assertTrue(false);
+  }
+
+  @Test
+  void withdrawMoney() {
+  }
+}
