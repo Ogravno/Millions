3566660eeaed15c28a8187467cd8a687c6ce7905
Odin Grav <odin@grav.name>
2026-03-19 02:54:02 +0100

    wrote unit tests for transaction classes

---
+++ src/test/java/edu/ntnu/idatt2003/group16/transaction/SaleTest.java
@@ -0,0 +1,11 @@
+package edu.ntnu.idatt2003.group16.transaction;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SaleTest {
+
+  @BeforeEach
+  void setUp() {
+    }
+}
