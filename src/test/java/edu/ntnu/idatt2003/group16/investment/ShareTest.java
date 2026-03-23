80d20016ce8153a4a7ad1087524126a3f2e72c77
Robin <robinsprestmo@gmail.com>
2026-03-23 14:59:38 +0100

    Updated javadoc in Share

---
+++ src/test/java/edu/ntnu/idatt2003/group16/investment/ShareTest.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idatt2003.group16.investment;
+
+import org.junit.jupiter.api.Nested;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShareTest {
+
+  @Nested
+  class constructorTests{
+
+  }
+
+  @Nested
+  class
+
+}
