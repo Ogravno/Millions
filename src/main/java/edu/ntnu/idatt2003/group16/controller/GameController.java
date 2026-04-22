4b5a60ce5d5fbe99c0dfc1eadf5070bde46356cf
Robin <robinsprestmo@gmail.com>
2026-04-22 10:08:53 +0200

    Added GameController

---
+++ src/main/java/edu/ntnu/idatt2003/group16/controller/GameController.java
@@ -0,0 +1,25 @@
+package edu.ntnu.idatt2003.group16.controller;
+
+import edu.ntnu.idatt2003.group16.model.GameSession;
+
+/**
+ * Controller that connects the GUI with the game logic.
+ */
+public class GameController {
+
+  private final GameSession gameSession;
+
+  public GameController(GameSession gameSession) {
+    if (gameSession == null) {
+      throw new IllegalArgumentException("GameSession cannot be null.");
+    }
+    this.gameSession = gameSession;
+  }
+
+  /**
+   * Advances the game by one week.
+   */
+  public void advanceWeek() {
+    gameSession.advanceWeek();
+  }
+}
