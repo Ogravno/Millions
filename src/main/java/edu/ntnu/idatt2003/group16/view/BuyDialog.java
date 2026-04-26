10e560c3f0e3e16c46297eea9dec11b558961a36
Robin <robinsprestmo@gmail.com>
2026-04-26 11:35:36 +0200

    Added calculator to buyDialog

--- src/main/java/edu/ntnu/idatt2003/group16/view/BuyDialog.java
+++ src/main/java/edu/ntnu/idatt2003/group16/view/BuyDialog.java
@@ -1,8 +1,10 @@
 package edu.ntnu.idatt2003.group16.view;
 
 import edu.ntnu.idatt2003.group16.controller.GameController;
+import edu.ntnu.idatt2003.group16.model.investment.Share;
 import edu.ntnu.idatt2003.group16.model.investment.Stock;
 import edu.ntnu.idatt2003.group16.model.transaction.Purchase;
+import edu.ntnu.idatt2003.group16.model.transaction.calculator.PurchaseCalculator;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
@@ -28,17 +30,59 @@ public class BuyDialog extends Dialog<Purchase> {
     setTitle("Buy Stock");
     setHeaderText("Buy " + stock.getSymbol());
 
+    getDialogPane().setPrefSize(300, 225);
+
     Label priceLabel = new Label("Price: " + stock.getCurrentPrice());
     Label playerBalance = new Label("Balance: " + gameController.getPlayerMoney());
+    Label totalPriceLabel = new Label("Total: 0");
+    Label grossPriceLabel = new Label();
+    Label feePriceLabel = new Label();
+
     TextField quantityField = new TextField();
     quantityField.setPromptText("Quantity");
+
+    quantityField.textProperty().addListener((observer, oldValue, newValue) -> {
+      try {
+        if (newValue.isBlank()) {
+          totalPriceLabel.setText("Total: 0");
+          grossPriceLabel.setText("");
+          feePriceLabel.setText("");
+        return;
+        }
+
+        BigDecimal quantity = new BigDecimal(newValue);
+
+        Share tempShare = new Share(
+          stock,
+          quantity,
+          stock.getCurrentPrice()
+        );
+
+        PurchaseCalculator calculator = new PurchaseCalculator(tempShare);
+
+        BigDecimal gross = calculator.calculateGross();
+        BigDecimal commission = calculator.calculateCommission();
+        BigDecimal total = calculator.calculateTotal();
+
+        totalPriceLabel.setText("Total: " + total);
+        grossPriceLabel.setText("Total stock price: " + gross);
+        feePriceLabel.setText("Fee: " + commission);
+
+      } catch (Exception e) {
+        totalPriceLabel.setText("Invalid quantity");
+      }
+    });
+
 
     VBox content = new VBox(10);
     content.getChildren().addAll(
       new Label("Stock: " + stock.getCompany()),
       priceLabel,
       playerBalance,
-      quantityField
+      quantityField,
+      totalPriceLabel,
+      grossPriceLabel,
+      feePriceLabel
     );
 
     ButtonType buyButtonType = new ButtonType("Buy");
