482c537634def8c1ef7a91bee312939193b1d045
Robin <robinsprestmo@gmail.com>
2026-05-26 20:43:03 +0200

    Updated Javadoc in RecieptDialog

--- src/main/java/edu/ntnu/idatt2003/group16/view/ReceiptDialog.java
+++ src/main/java/edu/ntnu/idatt2003/group16/view/ReceiptDialog.java
@@ -5,24 +5,30 @@ import edu.ntnu.idatt2003.group16.model.investment.Share;
 import edu.ntnu.idatt2003.group16.model.investment.Stock;
 import edu.ntnu.idatt2003.group16.model.transaction.Purchase;
 import edu.ntnu.idatt2003.group16.model.transaction.Transaction;
+import java.math.RoundingMode;
+import java.net.URL;
+import java.util.Optional;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-import java.math.RoundingMode;
-import java.net.URL;
-import java.util.Optional;
-
+/**
+ * Dialog for displaying a transaction receipt.
+ */
 public class ReceiptDialog extends Dialog<Void> {
 
+  /**
+   * Creates a receipt dialog for a transaction.
+   *
+   * @param appController the application controller
+   * @param transaction the transaction to display
+   */
   public ReceiptDialog(AppController appController, Transaction transaction) {
-    Share share = transaction.getShare();
-    Stock stock = share.getStock();
 
     setTitle("Receipt");
     setHeaderText("Receipt");
-    getDialogPane().setPrefSize(300,225);
+    getDialogPane().setPrefSize(300, 225);
 
     URL themeStyleSheet;
     if (appController.isDarkTheme()) {
@@ -39,39 +45,55 @@ public class ReceiptDialog extends Dialog<Void> {
       getDialogPane().getStylesheets().add(styleSheet.toExternalForm());
     }
 
-    Label purchaseOrSale = new Label(transaction.getClass().getSimpleName());
-    Label stockSymbolAndName = new Label("Stock: " + stock.getSymbol() + " " + stock.getCompany());
-    Label amountOfShares = new Label("Shares: " + share.getQuantity().toString());
-    Label pricePerShare = new Label("Price per share: " + share.getPurchasePrice().toString());
-    Label totalPrice = new Label("Total price for shares: " + transaction.getCalculator().calculateGross().toString());
-    Label totalFee = new Label("Fee: " + transaction.getCalculator().calculateCommission().setScale(2, RoundingMode.HALF_UP));
-    Label tax = new Label("Tax: " + transaction.getCalculator().calculateTax().setScale(2, RoundingMode.HALF_UP));
-    Label totalPriceWithFeeAndTax = new Label("Total price: " + transaction.getCalculator().calculateTotal().setScale(2, RoundingMode.HALF_UP));
-    Label week = new Label("Week: " + transaction.getWeek());
+    Share share = transaction.getShare();
+    Stock stock = share.getStock();
+
+    Label purchaseOrSale =
+        new Label(transaction.getClass().getSimpleName());
+    Label stockSymbolAndName =
+        new Label("Stock: " + stock.getSymbol() + " " + stock.getCompany());
+    Label amountOfShares =
+        new Label("Shares: " + share.getQuantity().toString());
+    Label pricePerShare =
+        new Label("Price per share: " + share.getPurchasePrice().toString());
+    Label totalPrice =
+        new Label("Total price for shares: "
+            + transaction.getCalculator().calculateGross().toString());
+    Label totalFee =
+        new Label("Fee: " + transaction.getCalculator().calculateCommission()
+          .setScale(2, RoundingMode.HALF_UP));
+    Label tax =
+        new Label("Tax: " + transaction.getCalculator().calculateTax()
+          .setScale(2, RoundingMode.HALF_UP));
+    Label totalPriceWithFeeAndTax =
+        new Label("Total price: " + transaction.getCalculator().calculateTotal()
+          .setScale(2, RoundingMode.HALF_UP));
+    Label week =
+        new Label("Week: " + transaction.getWeek());
 
     VBox content = new VBox(10);
     if (transaction instanceof Purchase) {
       content.getChildren().addAll(
-        purchaseOrSale,
-        stockSymbolAndName,
-        amountOfShares,
-        pricePerShare,
-        totalPrice,
-        totalFee,
-        totalPriceWithFeeAndTax,
-        week
+          purchaseOrSale,
+          stockSymbolAndName,
+          amountOfShares,
+          pricePerShare,
+          totalPrice,
+          totalFee,
+          totalPriceWithFeeAndTax,
+          week
       );
     } else {
       content.getChildren().addAll(
-        purchaseOrSale,
-        stockSymbolAndName,
-        amountOfShares,
-        pricePerShare,
-        totalPrice,
-        totalFee,
-        tax,
-        totalPriceWithFeeAndTax,
-        week
+          purchaseOrSale,
+          stockSymbolAndName,
+          amountOfShares,
+          pricePerShare,
+          totalPrice,
+          totalFee,
+          tax,
+          totalPriceWithFeeAndTax,
+          week
       );
     }
 
@@ -80,6 +102,11 @@ public class ReceiptDialog extends Dialog<Void> {
 
   }
 
+  /**
+   * Opens the dialog and waits for the user response.
+   *
+   * @return an empty optional when the dialog is closed
+   */
   public Optional<Void> showAndGetResult() {
     return showAndWait();
   }
