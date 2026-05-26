347755a35da75cedb1ffc22e4e01bd8925544388
Robin <robinsprestmo@gmail.com>
2026-05-26 20:22:25 +0200

    UpdatedJavadoc in HistoricalPricesDialog

--- src/main/java/edu/ntnu/idatt2003/group16/view/HistoricalPricesDialog.java
+++ src/main/java/edu/ntnu/idatt2003/group16/view/HistoricalPricesDialog.java
@@ -3,6 +3,10 @@ package edu.ntnu.idatt2003.group16.view;
 import edu.ntnu.idatt2003.group16.controller.AppController;
 import edu.ntnu.idatt2003.group16.model.exchange.Exchange;
 import edu.ntnu.idatt2003.group16.model.investment.Stock;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
@@ -10,26 +14,31 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-import java.math.BigDecimal;
-import java.net.URL;
-import java.util.List;
-import java.util.Optional;
-
+/**
+ * Dialog for displaying historical stock prices.
+ */
 public class HistoricalPricesDialog extends Dialog<Void> {
 
   private static final int itemsPerPage = 10;
 
-  private int currentpage = 0;
+  private int currentPage = 0;
   private final List<BigDecimal> historicalPriceList;
   private final VBox historicalPrices = new VBox(10);
   private final Label pageLabel = new Label();
 
+  /**
+   * Creates a dialog for displaying historical stock prices.
+   *
+   * @param appController the application controller
+   * @param stock the stock to display
+   * @param exchange the exchange containing the stock
+   */
   public HistoricalPricesDialog(AppController appController, Stock stock, Exchange exchange) {
     this.historicalPriceList = stock.getHistoricalPrices();
 
     setTitle("Stock");
     setHeaderText(stock.getCompany());
-    getDialogPane().setPrefSize(300,225);
+    getDialogPane().setPrefSize(300, 225);
 
     URL themeStyleSheet;
     if (appController.isDarkTheme()) {
@@ -46,23 +55,19 @@ public class HistoricalPricesDialog extends Dialog<Void> {
       getDialogPane().getStylesheets().add(styleSheet.toExternalForm());
     }
 
-    Label currentPrice = new Label( "Current Price: " + stock.getCurrentPrice().toString());
-    Label highestPrice = new Label("Highest Price: " + stock.getHighestPrice().toString());
-    Label lowestPrice = new Label("Lowest Price: " + stock.getLowestPrice().toString());
-
     Button prevousButton = new Button("Previous");
     Button nextButton = new Button("Next");
 
     prevousButton.setOnAction(event -> {
-      if (currentpage > 0) {
-        currentpage--;
+      if (currentPage > 0) {
+        currentPage--;
         updateHistoricalPrices();
       }
     });
 
     nextButton.setOnAction(event -> {
-      if (currentpage < getTotalPages() -1) {
-        currentpage++;
+      if (currentPage < getTotalPages() - 1) {
+        currentPage++;
         updateHistoricalPrices();
       }
     });
@@ -71,23 +76,33 @@ public class HistoricalPricesDialog extends Dialog<Void> {
 
     updateHistoricalPrices();
 
+    Label currentPrice = new Label("Current Price: " + stock.getCurrentPrice().toString());
+    Label highestPrice = new Label("Highest Price: " + stock.getHighestPrice().toString());
+    Label lowestPrice = new Label("Lowest Price: " + stock.getLowestPrice().toString());
+
     VBox content = new VBox(10);
     content.getChildren().addAll(
-      currentPrice,
-      highestPrice,
-      lowestPrice,
-      historicalPrices,
-      pageButtons);
+        currentPrice,
+        highestPrice,
+        lowestPrice,
+        historicalPrices,
+        pageButtons);
 
     getDialogPane().setContent(content);
     getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
 
   }
 
+  /**
+   * Updates the displayed historical prices.
+   *
+   * <p>Prices are displayed in pages with the newest
+   * prices shown first.</p>
+   */
   public void updateHistoricalPrices() {
     historicalPrices.getChildren().clear();
 
-    int start = currentpage * itemsPerPage;
+    int start = currentPage * itemsPerPage;
     int end = Math.min(start + itemsPerPage, historicalPriceList.size());
 
     for (int i = start; i < end; i++) {
@@ -96,20 +111,24 @@ public class HistoricalPricesDialog extends Dialog<Void> {
       BigDecimal stockPrice = historicalPriceList.get(reversedIndex);
 
       Label price = new Label(
-        "Week: " + (reversedIndex + 1)
+          "Week: " + (reversedIndex + 1)
           + " Price: " + stockPrice
       );
 
       historicalPrices.getChildren().add(price);
     }
-    pageLabel.setText("Page " + (currentpage + 1) + " of " + getTotalPages());
+    pageLabel.setText("Page " + (currentPage + 1) + " of " + getTotalPages());
   }
 
   private int getTotalPages() {
     return (int) Math.ceil((double) historicalPriceList.size() / itemsPerPage);
   }
 
-
+  /**
+   * Opens the dialog and waits for the user response.
+   *
+   * @return an empty optional when the dialog is closed
+   */
   public Optional<Void> showAndGetResult() {
     return showAndWait();
   }
